2ae6e571e644f2c80a7e545747fa43d1e05669dc
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-08-03 08:38:55 -0300

    fix computerPlay, fix singleRound to call properly computerSelection and create the game function

--- scripts/script.js
+++ scripts/script.js
@@ -14,33 +14,53 @@ const computerPlay = () => {
 
 const singleRound = (playerSelection, computerSelection) => {
 	let result;
-	if (playerSelection === computerSelection) {
+	if (playerSelection === computerSelection()) {
 		return "It's a Draw"
 	} else {
 		if (playerSelection === 'Rock') {
-			if (computerSelection === 'Scissor') {
+			if (computerSelection() === 'Scissor') {
 				result = 'Win'
 			} else {
 				result = 'Loose'
 			}
 		} else if (playerSelection === 'Paper') {
-			if (computerSelection === 'Scissor') {
+			if (computerSelection() === 'Scissor') {
 				result = 'Win'
 			} else {
 				result = 'Loose'
 			}
 		}else if (playerSelection === 'Rock') {
-			if (computerSelection === 'Scissor') {
+			if (computerSelection() === 'Scissor') {
 				result = 'Win'
 			} else {
 				result = 'Loose'
 			}
 		}
 
-	return `You ${result}`		
+	return `You ${result}`
 	}
 }
 
-player = 'Rock';
-computerSelection = computerPlay();
+player = 'Paper';
+computerSelection = computerPlay;
 
+const game = (func, arg1, arg2) => {	
+	let counter = [0,0,0];
+	let round = 1
+	for (let i = 0; i < 5; i++) {
+		let current = func(arg1, arg2);
+		console.log(`round ${round++}: ${current}`);
+
+		if (current === "It's a Draw") {
+			counter[0] += 1;
+		} else if (current === "You Win") {
+			counter[1] += 1;
+		} else {
+			counter[2] += 1;
+		}
+	}
+	return `End of game: ${counter[0]} Draws, ${counter[1]} Wins and ${counter[2]} Losts`
+
+}
+
+console.log(game(singleRound, player, computerSelection))
