6d5200c900285668b03db53ecd99186afaf82b46
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-08-10 15:09:02 -0300

    added resultLogger, globalScoreUpdate, endGame and resetter ; global vars 'counter' and 'round'

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,69 @@
+let counter = [0, 0, 0];
+let round = 1;
+
 const computerPlay = () => {
     let computer = Math.floor(Math.random() * 3 + 1);
     return computer === 1 ? 'Paper' : computer === 2 ? 'Scissor' : 'Rock'
 }
 
-
 const singleRound = (playerSelection, computerSelection) => {
     let result;
     if (playerSelection === computerSelection) {
-        return `Both Played: ${playerSelection}.\nIt's a Draw`
+        return 'Draw'
     } else {
         if (playerSelection === 'Rock') {
-            if (computerSelection === 'Scissor') {
-                result = 'Win'
-            } else {
-                result = 'Loose'
-            }
+            result = computerSelection === 'Scissor' ? 'Win' : 'Loose';
         } else if (playerSelection === 'Paper') {
-            if (computerSelection === 'Rock') {
-                result = 'Win'
-            } else {
-                result = 'Loose'
-            }
+            result = computerSelection === 'Rock' ?  'Win' : 'Loose';
         } else if (playerSelection === 'Scissor') {
-            if (computerSelection === 'Paper') {
-                result = 'Win'
-            } else {
-                result = 'Loose'
-            }
-        }
-
-        return `You Played: ${playerSelection}.\nComputer Played: ${computerSelection}.\nResult: You ${result}`
-    }
-}
-
-let computerSelection = computerPlay();
-
-
-/*
-const game = (func, arg1, arg2) => {
-    let counter = [0, 0, 0];
-    let round = 1
-    for (let i = 0; i < 5; i++) {
-        let current = func(arg1(), arg2);
-        console.log(`round ${round++}: ${current}`);
-        if (current === "It's a Draw") {
-            counter[0] += 1;
-        } else if (current === "You Win") {
-            counter[1] += 1;
-        } else {
-            counter[2] += 1;
+            result = computerSelection === 'Paper' ?  'Win' : 'Loose';
         }
     }
-    return `End of game: ${counter[0]} Draws, ${counter[1]} Wins and ${counter[2]} Losts`
+    return result
 }
-*/
 
-function roundCaller(e) {
-    player = e.currentTarget.dataset.hand;
-    console.log(singleRound(player, computerSelection))
+function globalScoresUpdate(currentRound, counter) {
+    ++round;
+    pScore.textContent = `Round ${round}`
+    if (currentRound === 'Win') counter[0]++;
+    if (currentRound === 'Loose') counter[1]++;
+    if (currentRound === 'Draw') counter[2]++;
+}
+
+const resultLogger = text => {
+   pResult.textContent = text;
+}
+
+const resetter = () => {
+    counter = [0,0,0];
+    round = 0
+}
+
+const endGame = () => {
+    if (!(round === 5)) return;
+    endGameResult = counter[0] > counter[1] ? 'Win' : 'Lost'
+    if (confirm(`You ${endGameResult}! 
+        Press Ok if want to start again`)) {
+    resetter()    
+    }
 
     
 }
+
+
+function roundCaller(e) {
+    let player = e.currentTarget.dataset.hand;
+    let computerRound = computerPlay();
+    let currentRound = singleRound(player, computerRound)
+    let notDraw = `You Played: ${player}.\nComputer Played: ${computerRound}.\nResult: You ${currentRound}`
+    let pLog = currentRound === 'Draw' ? `Both Played: ${player}.\nIt's a Draw` : notDraw;
+    resultLogger(pLog);
+    globalScoresUpdate(currentRound, counter);
+    endGame()
+
+}
+const pResult = document.querySelector('#result');
+const pScore = document.querySelector('#score')
 const playerChoice = document.querySelectorAll('.hand');
 playerChoice.forEach( choice => choice.addEventListener('click', roundCaller))
 
-
-//singleRound(player, computerSelection)
